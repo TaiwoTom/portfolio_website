adb01474ec16e146cfa90a07ec1aa5191110a418
Taiwo Tom-Ayegunle <taiwotom-ayegunle@Taiwos-MacBook-Pro.local>
2022-11-03 20:12:05 +0100

    done

---
+++ all.js
@@ -0,0 +1,8 @@
+var btn = document.querySelector("button");
+btn.onclick = () => {
+  window.print();
+};
+
+alert(
+  "You can print the page by clicking on the button at the bottom right corner."
+);
